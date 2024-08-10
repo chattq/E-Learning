e75a0796fd14fc79782554f7260f080d3e3ea0af
Nguyễn Thị Bích Tuệ <95497729+Tue811@users.noreply.github.com>
2024-08-10 11:42:55 +0700

    update

---
+++ BackEnd/src/routes/blogs.routes.ts
@@ -0,0 +1,44 @@
+import { Router } from 'express'
+import blogController from '~/controllers/blogs.controllers'
+import { addNewBlogValidator } from '~/middlewares/blogs.middlewares'
+
+import { wrapRequestHandler } from '~/utils/handlers'
+
+const blogsRouter = Router()
+
+blogsRouter.get('/blog/getall', wrapRequestHandler(blogController.getAllBlogs))
+blogsRouter.post('/create', addNewBlogValidator, wrapRequestHandler(blogController.addNewBlog))
+
+// usersRouter.post(
+//   '/logout',
+//   accessTokenValidator,
+//   refreshTokenValidator,
+//   wrapRequestHandler(userController.logoutController)
+// )
+// /**
+//  * Description:
+//  * Path: /verify-email
+//  * Method: Post
+//  * Header:
+//  * Body: email_verify_token
+//  */
+// usersRouter.post('/verify-email', emailVerifyTokenValidator, wrapRequestHandler(userController.logoutController))
+
+// /**
+//  * Description: verify email when user client click on the link in email
+//  * Path: /resen-verify-email
+//  * Method: Post
+//  * Header: {Authorization: 'Bearer ' + <accessToken>}
+//  * Body: {}
+//  */
+// usersRouter.post('/resent-verify-email', accessTokenValidator, wrapRequestHandler(userController.logoutController))
+
+// /**
+//  * Description: get my profile
+//  * Path: /me
+//  * Method: Post
+//  * Header: {Authorization: 'Bearer ' + <accessToken>}
+//  */
+// usersRouter.post('/me', accessTokenValidator, wrapRequestHandler(userController.getMeController))
+
+export default blogsRouter
