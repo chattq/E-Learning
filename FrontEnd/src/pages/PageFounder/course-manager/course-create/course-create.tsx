fe6280643b2f65e3f9ba1bce4cbacb068b8c06ba
chattq <baotuyet927@gmail.com>
2024-08-27 12:00:23 +0700

    update

--- FrontEnd/src/pages/PageFounder/course-manager/course-create/course-create.tsx
+++ FrontEnd/src/pages/PageFounder/course-manager/course-create/course-create.tsx
@@ -8,30 +8,45 @@ import InforDetailCourse from "./form-detail-course/infor-detail-course";
 import CourseRequirements from "./course-requirements/course-requirements";
 import CourseDescription from "./course-description/course-description";
 import "./../course-manager.scss";
+import { useCallback, useState } from "react";
 
 export default function CourseCreate() {
+  const [CourseType, setCourseType] = useState("");
+  const onChangeCourseType = useCallback(
+    (value: any) => {
+      setCourseType(value);
+    },
+    [CourseType]
+  );
   return (
     <AdminPageLayout>
       <div className="px-4 pt-[14px] w-full">
         <CardLayout title={"Thông tin cơ bản"}>
-          <InforBaseCourse />
+          <InforBaseCourse onChangeCourseType={onChangeCourseType} />
         </CardLayout>
-        <CardLayout title={"Chi tiết khóa học"}>
-          <InforDetailCourse />
-          <CourseRequirements />
-          <CourseDescription />
-        </CardLayout>
-        <CardLayout title={"Nội dung khóa học"}>
-          <InforContentCourse />
-        </CardLayout>
+        {CourseType !== "" && (
+          <>
+            <CardLayout title={"Chi tiết khóa học"}>
+              <InforDetailCourse />
+              <CourseRequirements />
+              <CourseDescription />
+            </CardLayout>
+            {CourseType !== "online" && (
+              <CardLayout title={"Nội dung khóa học"}>
+                <InforContentCourse />
+              </CardLayout>
+            )}
+          </>
+        )}
         <div className="h-[63px]"></div>
         <CardLayout
           className={
-            "pb-[14.5px] fixed bottom-0 z-10 mb-[0px] right-[15px] left-[266px] boxShadowButton rounded-none"
+            "pb-[14.5px] fixed bottom-0 z-10 mb-[0px] right-[15px] left-[275px] boxShadowButton rounded-none"
           }>
           <div className="flex gap-4 justify-end">
-            <Button color="red">Lưu</Button>
-            <Button color="red">Hủy</Button>
+            <Button color="red">Primary</Button>
+            <Button color="red">Primary</Button>
+            <Button color="red">Primary</Button>
           </div>
         </CardLayout>
       </div>
