34e1e76e7f0e942c813ec5542fa6a03ee64f2667
Trịnh Quang <113630057+chattq@users.noreply.github.com>
2024-11-07 22:02:30 +0700

    Delete BackEnd/dist directory

--- BackEnd/dist/config/vietQr.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.vietQR = void 0;
-const vietqr_1 = require("vietqr");
-exports.vietQR = new vietqr_1.VietQR({
-    clientID: 'de8a0804-a76d-41e5-8ad6-31503ce7d5f4',
-    apiKey: '17c29f09-4ea2-4417-b9c2-7f020d35de42'
-});
